2484ac27e78b74b87c3c7bbd40850fc53668cb4b
Brian To <brian17898@gmail.com>
2020-09-03 04:00:58 -0700

    Removed verbose unpack folder, added support for default installs of GoG

--- nasher_unpack_folder_verbose.bat
+++
@@ -1,2 +0,0 @@
-call docker run --rm -it -v "%cd%":/nasher nwntools/nasher:latest unpack --file:modules/and_the_Wailing_Death --removeDeleted
-PAUSE
--- toolset.bat
+++ toolset.bat
@@ -1,9 +1,13 @@
 if exist "C:\Program Files (x86)\Steam\steamapps\common\Neverwinter Nights\bin\win32\nwtoolset.exe" goto steam
 
+if exist "C:\Program Files (x86)\GOG Galaxy\Games\Neverwinter Nights Enhanced Edition\bin\win32\nwtoolset.exe" goto gog
+
 :steam
 START "" "C:\Program Files (x86)\Steam\steamapps\common\Neverwinter Nights\bin\win32\nwtoolset.exe" -userdirectory "%cd%"
 
 :gog
+START "" "C:\Program Files (x86)\GOG Galaxy\Games\Neverwinter Nights Enhanced Edition\bin\win32\nwtoolset.exe" -userdirectory "%cd%"
+
 
 :bdc
 
