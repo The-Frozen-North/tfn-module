6d369846628b3d9a22eed6922233f5eec1221fef
Brian To <brian17898@gmail.com>
2020-09-04 01:59:32 -0700

    Added support for finding toolset installs through registry for steam and gog

--- toolset.bat
+++ toolset.bat
@@ -1,13 +1,38 @@
-if exist "C:\Program Files (x86)\Steam\steamapps\common\Neverwinter Nights\bin\win32\nwtoolset.exe" goto steam
+@echo off
 
-if exist "C:\Program Files (x86)\GOG Galaxy\Games\Neverwinter Nights Enhanced Edition\bin\win32\nwtoolset.exe" goto gog
+set DefaultSteamPath=C:\Program Files (x86)\Steam\steamapps\common\Neverwinter Nights\bin\win32\nwtoolset.exe
+if exist "%DefaultSteamPath%" (
+	echo DefaultSteamPath found: "%DefaultSteamPath%"
+	START "" "%DefaultSteamPath%" -userdirectory "%cd%"
+	exit
+)
 
-:steam
-START "" "C:\Program Files (x86)\Steam\steamapps\common\Neverwinter Nights\bin\win32\nwtoolset.exe" -userdirectory "%cd%"
+set DefaultGogPath=C:\Program Files (x86)\GOG Galaxy\Games\Neverwinter Nights Enhanced Edition\bin\win32\nwtoolset.exe
+if exist "%DefaultGogPath%" (
+	echo DefaultGogPath found: "%DefaultGogPath%"
+	START "" "%DefaultGogPath%" -userdirectory "%cd%"
+	exit
+)
 
-:gog
-START "" "C:\Program Files (x86)\GOG Galaxy\Games\Neverwinter Nights Enhanced Edition\bin\win32\nwtoolset.exe" -userdirectory "%cd%"
+set DefaultBeamdogPath=C:/Users/%USERNAME%/Beamdog/7710
 
+(for /f "usebackq tokens=1,2,*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam" /v UninstallString`) do set SteamPath32=%%c)>nul 2>&1
+(for /f "usebackq tokens=1,2,*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam" /v UninstallString`) do set SteamPath64=%%c)>nul 2>&1
+set SteamPath=%SteamPath64%%SteamPath32%
+set SteamPath=%SteamPath:\uninstall.exe=%
+set SteamPath=%SteamPath%\steamapps\common\Neverwinter Nights\bin\win32\nwtoolset.exe
+if exist "%SteamPath%" (
+	echo Steam Path found: "%SteamPath%"
+	START "" "%SteamPath%" -userdirectory "%cd%"
+	exit
+)
 
-:bdc
+(for /f "usebackq tokens=1,2,*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\GOG.com\Games\1097893768" /v PATH`) do set GogPath=%%c)>nul 2>&1
+set GogPath=%GogPath%\bin\win32\nwtoolset.exe
 
+if exist "%GogPath%" (
+	echo GoG path found: "%GogPath%"
+	START "" "%GogPath%" -userdirectory "%cd%"
+	exit
+)
+pause
