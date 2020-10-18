b234003d4dc6f1beedb72e17e93dd5a9a1a21890
Brian To <brian17898@gmail.com>
2020-10-18 06:58:24 -0700

    Added force refreshing of cache on nasher unpack, reduced sludge volume on sewers

--- git_refresh_cache.bat
+++
@@ -1,2 +0,0 @@
-git rm --cached src -r
-git add .
--- nasher_unpack_folder.bat
+++ nasher_unpack_folder.bat
@@ -1,1 +1,3 @@
 call docker run --rm -it -v "%cd%":/nasher nwntools/nasher:latest unpack --file:modules/and_the_Wailing_Death --removeDeleted
+git rm --cached src -r
+git add .
