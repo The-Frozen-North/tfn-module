efbcf1c0209967d754f90e4c27fd309457da281f
Brian To <brian17898@gmail.com>
2022-04-17 19:00:49 -0700

    run script tweaks + echo message

--- linux_run_server.sh
+++ linux_run_server.sh
@@ -4,10 +4,17 @@ git pull
 
 git -C nwn-assets pull || git clone https://github.com/urothis/nwn-assets.git nwn-assets
 
+echo
+echo "Checking to see if there is a previous module. If prompted, type 'Y' to delete the current module and module folder, or 'N' to cancel." 
+echo "If you are canceling, you can close the window and exit. nasher will not continue if there is a module file and folder."
+echo "It will automatically continue if you do not have module built (clean slate)" 
+echo
+echo "WARNING: 'Y' will delete all unsaved changes! Commit or stash them before continuing."
+
 rm -d -RI modules
 rm and_the_Wailing_Death.mod
 
-$PWD/tools/linux/nasher/nasher install  --verbose --erfUtil:"$PWD/tools/linux/neverwinter/nwn_erf" --gffUtil:"$PWD/tools/linux/neverwinter/nwn_gff" --tlkUtil:"$PWD/tools/linux/neverwinter/nwn_tlk" --nssCompiler:"$PWD/tools/linux/nwnsc/nwnsc" --installDir:"$PWD" --nssFlags:"-oe -i $PWD/nwn-assets/nwscript" --no
+$PWD/tools/linux/nasher/nasher install  --debug --erfUtil:"$PWD/tools/linux/neverwinter/nwn_erf" --gffUtil:"$PWD/tools/linux/neverwinter/nwn_gff" --tlkUtil:"$PWD/tools/linux/neverwinter/nwn_tlk" --nssCompiler:"$PWD/tools/linux/nwnsc/nwnsc" --installDir:"$PWD" --nssFlags:"-oe -i $PWD/nwn-assets/nwscript" --yes
 
 rm server/config/common.env
 rm server/modules/and_the_Wailing_Death.mod
--- win_nasher_install.bat
+++ win_nasher_install.bat
@@ -1,5 +1,14 @@
 git -C nwn-assets pull || git clone https://github.com/urothis/nwn-assets.git nwn-assets
 
+@echo off
+echo.
+echo Checking to see if there is a previous module. If prompted, type 'Y' to delete the current module and module folder, or 'N' to cancel. 
+echo If you are canceling, you can close the window and exit. nasher will not continue if there is a module file and folder.
+echo It will automatically continue if you do not have module built (clean slate) 
+echo.
+echo WARNING: 'Y' will delete all unsaved changes! Commit or stash them before continuing.
+@echo on
+
 del modules /S
 rd modules\and_the_Wailing_Death
 rd modules
