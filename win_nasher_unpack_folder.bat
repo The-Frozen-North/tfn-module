902cad907d70ee82cc80323b9c05e01bf175f926
b5635 <brian17898@gmail.com>
2024-05-24 00:13:52 -0700

    Update for Windows, need to update for Linux

--- win_nasher_unpack_folder.bat
+++ win_nasher_unpack_folder.bat
@@ -1,3 +1,3 @@
-"%CD%/tools/win/nasher/nasher.exe" unpack --file:modules/TFN --removeDeleted --erfUtil:"%CD%/tools/win/neverwinter64/nwn_erf.exe" --gffUtil:"%CD%/tools/win/neverwinter64/nwn_gff.exe" --tlkUtil:"%CD%/tools/win/neverwinter64/nwn_tlk.exe" --nssFlags:"-l"
+"%CD%/tools/win/nasher/nasher.exe" unpack --file:.build/modules/TFN --removeDeleted --erfUtil:"%CD%/tools/win/neverwinter64/nwn_erf.exe" --gffUtil:"%CD%/tools/win/neverwinter64/nwn_gff.exe" --tlkUtil:"%CD%/tools/win/neverwinter64/nwn_tlk.exe" --nssFlags:"-l"
 git rm --cached src -r
 git add .
