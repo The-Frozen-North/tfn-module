4415cc448f31775d98194865d4247f94733ba985
b5635 <brian17898@gmail.com>
2024-11-16 16:19:00 -0800

    Updated windows batch scripts to be more user friendly and catch some common errors, added separate script for pulling latest build + running server

--- win_run_server_dev_seed.bat
+++ win_run_server_dev_seed.bat
@@ -1,4 +1,32 @@
 @echo off
+
+IF NOT EXIST .build\modules\TFN.mod (
+    echo .build\modules\TFN.mod does not exist. Please run win_nasher_install.bat first to install the module
+    pause
+    exit /b 1
+)
+
+python --version > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Python is not installed. Please install Python to continue seeding the server.
+    pause
+    exit /b 1
+)
+
+docker --version > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker is not installed. Please install Docker to run the server.
+    pause
+    exit /b 1
+)
+
+docker info > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker error encountered or the Docker service isn't running! Please check and try again.
+    pause
+    exit /b 1
+)
+
 setlocal
 
 :: Capture start time
